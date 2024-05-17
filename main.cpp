0552fa3645aca97c68a08d8f4b431d2ba4e477d2
NextStep-IM <malikharis2003@gmail.com>
2024-05-17 23:36:48 +0500

    Move function definition to the botton and prototype to top

--- main.cpp
+++ main.cpp
@@ -9,177 +9,18 @@
 #include <linux/limits.h>
 namespace fs = std::filesystem;
 
-char **get_input(char *input)
-{
-    char **command = new char *[8];
-    const char *separator = " ";
-    char *parsed;
-    int index = 0;
 
-    parsed = strtok(input, separator); // strtok returns pointer to next token
-    while (parsed != NULL)
-    {
-        command[index] = parsed;
-        index++;
+char **get_input(char *input);
+int cd(char *path);
+int cfile(char *fileName);
+void crtdir(char *dir_path);
+void paw(char *rd_file);
+void exp(fs::path target);
+void del(fs::path target);
+void cpy(fs::path src, fs::path dest);
+void mov(fs::path old_path, fs::path new_path);
+void help();
 
-        // NULL argument instructs compiler to use source c-string
-        parsed = strtok(NULL, separator); // Gets the next token i.e. the token before second separator
-    }
-
-    command[index] = NULL;
-
-    return command;
-}
-
-int cd(char *path)
-{
-    return chdir(path);
-}
-
-// argc (size of command array) is problematic.
-int cfile(char *fileName)
-{
-    std::ofstream outputFile(fileName);
-    if (outputFile.is_open())
-    {
-        outputFile.close();
-        return 1;
-    }
-    else
-    {
-        return -1;
-    }
-}
-
-// Maybe add error_code object?
-void crtdir(char *dir_path)
-{
-    if (!fs::exists(dir_path))
-    {
-        fs::create_directories(dir_path);
-        std::cout << "Directory: " << dir_path << "\n";
-    }
-    else
-    {
-        std::cout << "Directory already exists\n";
-    }
-}
-
-void paw(char *rd_file)
-{
-    std::cout << "Content of " << rd_file << "\n";
-    std::string text;
-    std::ifstream readFile(rd_file);
-    while (getline(readFile, text))
-    {
-        std::cout << text << "\n";
-    }
-    readFile.close();
-}
-
-void exp(fs::path &target)
-{
-    // Iterates through the contents of the target directory (non-recursively)
-    for (auto const &dir_entry : fs::directory_iterator(target))
-    {
-        if (dir_entry.is_regular_file())
-        {
-            std::cout << dir_entry.path().filename().string() << "\n";
-        }
-        else if (dir_entry.is_directory())
-        {
-            std::cout << "\033[1;34m" << dir_entry.path().filename().string() << "\033[0m\n";
-        }
-    }
-}
-
-void del(fs::path target)
-{
-    std::error_code eCode;
-    if (fs::is_regular_file(target))
-    {
-        fs::remove(target);
-    }
-    else if (fs::is_directory(target))
-    {
-        fs::remove_all(target);
-    }
-    else
-    {
-        std::cout << "Error: " << target << " does not exist." << "\n";
-    }
-}
-
-// Issue: can't copy directory as a whole, only its contents and sub-directories
-void cpy(fs::path &src, fs::path &dest)
-{
-    std::error_code ec;
-    if (fs::is_regular_file(src))
-    {
-        fs::copy(src, dest, fs::copy_options::skip_existing, ec);
-        if (ec)
-        {
-            std::cerr << "Error copying file: " << ec.message() << "\n";
-        }
-        else
-        {
-            std::cout << "Success: File copied to " << dest << "\n";
-        }
-    }
-    else if (fs::is_directory(src))
-    {
-        fs::copy(src, dest, fs::copy_options::recursive | fs::copy_options::skip_existing, ec);
-        if (ec)
-        {
-            std::cerr << "Error copying directory: " << ec.message() << "\n";
-        }
-        else
-        {
-            std::cout << "Success: Directory copied to " << dest << "\n";
-        }
-    }
-}
-
-void mov(fs::path old_path, fs::path new_path)
-{
-    std::error_code ec;
-    if (fs::is_regular_file(old_path) && fs::is_directory(new_path))
-    {
-        cpy(old_path, new_path);
-        del(old_path);
-    }
-    else if (fs::is_directory(old_path) && fs::is_directory(new_path) && fs::exists(new_path))
-    {
-        fs::path combinedPath = new_path / old_path.filename();
-        fs::create_directory(combinedPath);
-        cpy(old_path, combinedPath);
-        del(old_path);
-    }
-    else
-    {
-        fs::rename(old_path, new_path, ec);
-        if (ec)
-        {
-            std::cerr << ec.message() << "\n";
-        }
-        else
-        {
-            std::cout << old_path.filename() << " renamed to " << new_path.filename() << "\n";
-        }
-    }
-}
-
-void help()
-{
-    std::cout << "exp    -    List directory content\n"
-              << "paw    -    Display file content\n"
-              << "cpy    -    Copy files and directories\n"
-              << "crtdir -    Create directories\n"
-              << "cfile  -    Create files\n"
-              << "cd     -    Change directory\n"
-              << "del    -    Delete files and directories\n"
-              << "mov    -    Move files and directories\n";
-}
 
 int main()
 {
@@ -350,4 +191,190 @@ int main()
     }
 
     return 0;
+}
+
+
+char **get_input(char *input)
+{
+    char **command = new char *[8];
+    const char *separator = " ";
+    char *parsed;
+    int index = 0;
+
+    parsed = strtok(input, separator); // strtok returns pointer to next token
+    while (parsed != NULL)
+    {
+        command[index] = parsed;
+        index++;
+
+        // NULL argument instructs compiler to use source c-string
+        parsed = strtok(NULL, separator); // Gets the next token i.e. the token before second separator
+    }
+
+    command[index] = NULL;
+
+    return command;
+}
+
+int cd(char *path)
+{
+    return chdir(path);
+}
+
+// argc (size of command array) is problematic.
+int cfile(char *fileName)
+{
+    std::ofstream outputFile(fileName);
+    if (outputFile.is_open())
+    {
+        outputFile.close();
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+// Maybe add error_code object?
+void crtdir(char *dir_path)
+{
+    if (!fs::exists(dir_path))
+    {
+        fs::create_directories(dir_path);
+        std::cout << "Directory: " << dir_path << "\n";
+    }
+    else
+    {
+        std::cout << "Directory already exists\n";
+    }
+}
+
+void paw(char *rd_file)
+{
+    std::cout << "Content of " << rd_file << "\n";
+    std::string text;
+    std::ifstream readFile(rd_file);
+    while (getline(readFile, text))
+    {
+        std::cout << text << "\n";
+    }
+    readFile.close();
+}
+
+void exp(fs::path target)
+{
+    // Iterates through the contents of the target directory (non-recursively)
+    for (auto dir_entry : fs::directory_iterator(target))
+    {
+        if (dir_entry.is_regular_file())
+        {
+            std::cout << dir_entry.path().filename().string() << "\n";
+        }
+        else if (dir_entry.is_directory())
+        {
+            std::cout << "\033[1;34m" << dir_entry.path().filename().string() << "\033[0m\n";
+        }
+    }
+}
+
+void del(fs::path target)
+{
+    std::error_code eCode;
+    if (fs::is_regular_file(target))
+    {
+        fs::remove(target, eCode);
+        if (eCode)
+        {
+            std::cerr << "Error: " << eCode.message() << "\n";
+        }
+    }
+    else if (fs::is_directory(target))
+    {
+        fs::remove_all(target, eCode);
+        if (eCode)
+        {
+            std::cerr << "Error: " << eCode.message() << "\n";
+        }
+    }
+    /* else
+    {
+        std::cerr << "Error: " << target << " does not exist." << "\n";
+    } */
+}
+
+// Issue: can't copy directory as a whole, only its contents and sub-directories
+void cpy(fs::path src, fs::path dest)
+{
+    if (!fs::exists(dest))
+    {
+        std::cerr << "Error: " << dest << " does not exist." << "\n";
+        return;
+    }
+    if (!fs::exists(src))
+    {
+        std::cerr << "Error: " << src << " does not exist." << "\n";
+        return;
+    }
+    std::error_code eCode;
+    if (fs::is_regular_file(src))
+    {
+        fs::copy(src, dest, eCode);
+        if (eCode)
+        {
+            std::cerr << "Error: " << eCode.message() << "\n";
+            return;
+        }
+
+    }
+    else if (fs::is_directory(src))
+    {
+        fs::copy(src, dest, fs::copy_options::recursive, eCode);
+        if (eCode)
+        {
+            std::cerr << "Error: " << eCode.message() << "\n";
+            return;
+        }
+    }
+}
+
+void mov(fs::path old_path, fs::path new_path)
+{
+    std::error_code ec;
+    if (fs::is_regular_file(old_path) && fs::is_directory(new_path))
+    {
+        cpy(old_path, new_path);
+        del(old_path);
+    }
+    else if (fs::is_directory(old_path) && fs::is_directory(new_path) && fs::exists(new_path))
+    {
+        fs::path combinedPath = new_path / old_path.filename();
+        fs::create_directory(combinedPath);
+        cpy(old_path, combinedPath);
+        del(old_path);
+    }
+    else
+    {
+        fs::rename(old_path, new_path, ec);
+        if (ec)
+        {
+            std::cerr << ec.message() << "\n";
+        }
+        else
+        {
+            std::cout << old_path.filename() << " renamed to " << new_path.filename() << "\n";
+        }
+    }
+}
+
+void help()
+{
+    std::cout << "exp    -    List directory content\n"
+              << "paw    -    Display file content\n"
+              << "cpy    -    Copy files and directories\n"
+              << "crtdir -    Create directories\n"
+              << "cfile  -    Create files\n"
+              << "cd     -    Change directory\n"
+              << "del    -    Delete files and directories\n"
+              << "mov    -    Move files and directories\n";
 }
