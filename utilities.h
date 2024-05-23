9030430cf481bebe7c07b23acc091f0cb3a412e0
NextStep-IM <malikharis2003@gmail.com>
2024-05-23 12:29:22 +0500

    CamelCase and removing identifiers

--- utilities.h
+++ utilities.h
@@ -3,16 +3,16 @@
 namespace fs = std::filesystem;
 // Function Prototypes
 
-std::vector<std::string> getInput(std::string input);
-int cd(fs::path dir_path);
-int cfile(fs::path fileName);
-void crtdir(fs::path dir_path);
-void paw(fs::path readFile);
-void exp(fs::path target);
-void del(fs::path target);
-void cpy(fs::path src, fs::path dest);
-void mov(fs::path old_path, fs::path new_path);
+std::vector<std::string> getInput(std::string);
+int cd(fs::path);
+int cfile(fs::path);
+void crtdir(fs::path);
+void paw(fs::path);
+void exp(fs::path);
+void del(fs::path);
+void cpy(fs::path, fs::path);
+void mov(fs::path, fs::path);
 void help();
-void cmdCheck(std::vector<std::string> command);
+void cmdCheck(std::vector<std::string>);
 
 #endif
